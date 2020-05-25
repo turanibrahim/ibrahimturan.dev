31449bb4d67593ac4736565b345ac54f4a9afb57
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-05-26 02:05:15 +0300

    contact page social media icon list item

---
+++ components/ContactSocialMedia.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="text-center mx-4 my-3">
+    <v-icon x-large :color="color" @click="redirectTo(link)">
+      {{ icons.get(name).path }}
+    </v-icon>
+  </div>
+</template>
+
+<script>
+const simpleIcons = require('simple-icons')
+
+export default {
+  props: {
+    name: String,
+    color: String,
+    link: String
+  },
+  data() {
+    return {
+      icons: simpleIcons
+    }
+  },
+  methods: {
+    redirectTo(link) {
+      window.open(link, '_blank')
+    }
+  }
+}
+</script>
