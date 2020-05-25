94535bd6787df6c6ec12b59758b5b20323a1780d
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-05-26 02:05:42 +0300

    blog page added

---
+++ pages/Blog.vue/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row no-gutters justify="center" align="center" class="fill-height">
+    <v-col cols="auto">
+      <h1 class="display-4 font-weight-black">Will be here ASAP</h1>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  //
+}
+</script>
