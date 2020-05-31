8882fc7bb0886e684555356919219aa8edf05266
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-05-31 04:04:46 +0300

    static data removed to store, fetch actions and mutations added

--- store/index.js
+++ store/index.js
@@ -1,5 +1,6 @@
 export const state = () => ({
-  //
+  locales: ['en', 'tr'],
+  locale: 'en'
 })
 
 export const getters = {
@@ -7,7 +8,11 @@ export const getters = {
 }
 
 export const mutations = {
-  //
+  SET_LANG(state, locale) {
+    if (state.locales.includes(locale)) {
+      state.locale = locale
+    }
+  }
 }
 
 export const actions = {
