cea8f8a30e325de5909e34109761b1eea95d8fb5
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-05-31 04:04:40 +0300

    static data removed to store, fetch actions and mutations added

---
+++ store/experience.js
@@ -0,0 +1,32 @@
+export const state = () => ({
+  experiences: [],
+  loading: false
+})
+
+export const getters = {
+  //
+}
+
+export const mutations = {
+  SET_EXPERIENCES(state, payload) {
+    state.experiences = payload
+  },
+  SET_LOADING(state, payload) {
+    state.loading = payload
+  }
+}
+
+export const actions = {
+  async fetchExperiences({ commit, rootState }) {
+    commit('SET_LOADING', true)
+    await this.$axios
+      .$get(`/api/experience/${rootState.locale}`)
+      .then((response) => {
+        commit('SET_EXPERIENCES', response)
+        commit('SET_LOADING', false)
+      })
+      .catch((err) => {
+        commit('SET_EXPERIENCES', err)
+      })
+  }
+}
