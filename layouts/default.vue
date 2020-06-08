d9173cf62bae2a376b824216ced26490a1488f59
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-06-09 01:02:33 +0300

    changed sidebar integrated to page and some visual changes made

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,112 +1,73 @@
 <template>
   <v-app>
     <div v-if="!loading" style="height:100vh">
-      <v-navigation-drawer
-        v-show="$nuxt.$vuetify.breakpoint.smAndDown"
-        v-model="drawer"
-        absolute
-        temporary
-      >
-        <LayuoutSidebar :menu-items="menuItems" :social-medias="socialMedias" />
-      </v-navigation-drawer>
+      <layuout-sidebar
+        v-if="$nuxt.$vuetify.breakpoint.smAndDown"
+        :menu-items="menuItems"
+        :social-medias="socialMedias"
+      />
       <v-container
         fluid
         class="fill-height"
         :class="$nuxt.$vuetify.breakpoint.smAndDown ? 'py-0' : ''"
       >
-        <v-row align-center justify-center row class="fill-height">
+        <v-row
+          align-center
+          justify-center
+          row
+          class="fill-height"
+          style="overflow-y:auto"
+        >
           <v-col
-            v-show="$nuxt.$vuetify.breakpoint.smAndDown"
-            dark
-            cols="12"
-            :style="$nuxt.$vuetify.breakpoint.smAndDown ? 'height:10%' : ''"
-          >
-            <v-toolbar class="ma-0 pa-0" tile>
-              <v-toolbar-title class="pa-0 ma-0">
-                <h1 class="headline font-weight-bold">
-                  {{ pageTitle }}
-                </h1>
-              </v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-app-bar-nav-icon
-                @click="drawer = !drawer"
-              ></v-app-bar-nav-icon>
-            </v-toolbar>
-          </v-col>
-          <v-col
-            v-if="$nuxt.$vuetify.breakpoint.mdAndUp"
-            v-show="!isHidden"
-            md="4"
-            lg="3"
-            xl="2"
-            class="fill-height"
+            v-if="$nuxt.$vuetify.breakpoint.smAndUp"
+            md="auto"
+            lg="auto"
+            xl="auto"
             transition="scroll-x-transition"
           >
             <layuout-sidebar
+              v-if="$nuxt.$vuetify.breakpoint.smAndUp"
               :menu-items="menuItems"
               :social-medias="socialMedias"
             />
           </v-col>
           <v-col
-            :md="!isHidden ? 8 : 'grow'"
-            :lg="!isHidden ? 9 : 'grow'"
-            :xl="!isHidden ? 10 : 'grow'"
+            md="grow"
+            lg="grow"
+            xl="grow"
             class="fill-height"
-            :style="$nuxt.$vuetify.breakpoint.smAndDown ? 'height:90%' : ''"
+            :style="
+              $nuxt.$vuetify.breakpoint.smAndDown
+                ? 'height:90%'
+                : 'display: flex; flex-direction: column; align-items: stretch;'
+            "
           >
-            <v-row class="fill-height" no-gutters>
-              <v-col
-                v-show="$nuxt.$vuetify.breakpoint.mdAndUp"
-                cols="12"
-                style="height: 10%;"
+            <h1 v-if="!isMiniVariant" class="display-2 font-weight-bold pb-3">
+              <v-icon
+                v-if="$nuxt.$vuetify.breakpoint.smAndDown"
+                @click="changeSideVisibility(true)"
               >
-                <v-row justify="end" align="end" no-gutters class="fill-height">
-                  <v-col align-self="center">
-                    <h1 class="display-2 font-weight-bold">
-                      {{ pageTitle }}
-                    </h1>
-                  </v-col>
-                  <v-col cols="auto" align-self="center">
-                    <v-app-bar-nav-icon
-                      x-large
-                      @click="isHidden = !isHidden"
-                    ></v-app-bar-nav-icon>
-                  </v-col>
-                </v-row>
+                mdi-chevron-right
+              </v-icon>
+              {{ pageTitle }}
+            </h1>
+            <v-card
+              elevation="24"
+              style="overflow-y: scroll"
+              class="fill-height"
+            >
+              <router-view></router-view>
+            </v-card>
+            <v-footer
+              padless
+              class="pa-1 mt-2 justify-end align-end"
+              elevation="24"
+            >
+              <v-col class="text-center caption py-0 my-0" cols="12">
+                {{ new Date().getFullYear() }} —
+                <strong>İbrahim Turan</strong>
               </v-col>
-              <v-col
-                cols="12"
-                :style="
-                  $nuxt.$vuetify.breakpoint.mdAndUp
-                    ? 'height:85%'
-                    : 'height:95%'
-                "
-              >
-                <v-card
-                  class="fill-height"
-                  elevation="24"
-                  style="overflow-y: auto;position: relative"
-                >
-                  <router-view></router-view>
-                </v-card>
-              </v-col>
-              <v-col
-                cols="12"
-                style="height:5%"
-                :class="$nuxt.$vuetify.breakpoint.smAndDown ? 'mt-1' : ''"
-              >
-                <v-row justify="end" align="end" no-gutters class="fill-height">
-                  <v-col>
-                    <v-footer padless class="pa-1" elevation="24">
-                      <v-col class="text-center caption py-0 my-0" cols="12">
-                        {{ new Date().getFullYear() }} —
-                        <strong>İbrahim Turan</strong>
-                      </v-col>
-                    </v-footer>
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
+            </v-footer>
           </v-col>
         </v-row>
       </v-container>
@@ -129,7 +90,7 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions, mapMutations } from 'vuex'
 import LayuoutSidebar from '../components/LayuoutSidebar.vue'
 
 export default {
@@ -140,16 +101,16 @@ export default {
     return {
       isDark: false,
       whichFlag: true,
-      drawer: false,
-      loading: true,
-      isHidden: false
+      loading: true
     }
   },
   computed: {
     ...mapState({
       menuItems: (state) => state.layout.menuItems,
       pageTitle: (state) => state.layout.pageTitle,
-      socialMedias: (state) => state.layout.socialMedias
+      socialMedias: (state) => state.layout.socialMedias,
+      isMiniVariant: (state) => state.layout.isMiniVariant,
+      showSideBar: (state) => state.layout.showSideBar
     })
   },
   created() {
@@ -160,6 +121,10 @@ export default {
   methods: {
     ...mapActions({
       fetchSocialMedias: 'layout/fetchSocialMedias'
+    }),
+    ...mapMutations({
+      chaneMiniVariant: 'layout/SET_MINIVARIANT',
+      changeSideVisibility: 'layout/SET_SIDEBAR_VISIBILITY'
     })
   }
 }
