f92b6fdf78d2a5672b482090c3431dd3d15bc29c
İbrahim Turan <ibrahimturan002@gmail.com>
2022-01-01 14:23:32 +0300

    create VLink component

---
+++ src/components/atoms/VLink.vue
@@ -0,0 +1,32 @@
+<template>
+  <component :is="component"
+    :href="props.href"
+    class="inline-flex justify-center hover:bg-indigo-100 p-1 font-bold rounded"
+    target="_blank"
+  >
+    <slot></slot>
+  </component>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  href: {
+    type: String,
+    required: true,
+  },
+  isLink: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const component = props.isLink ? 'a' : 'button';
+</script>
+
+<style lang="scss">
+.v-link {
+  @apply inline-flex justify-center hover:bg-indigo-100 p-1 font-bold rounded
+}
+</style>
