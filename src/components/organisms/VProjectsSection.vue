43f2a0f09c4625d7bc24e0035d53b814699e1dde
İbrahim Turan <ibrahimturan002@gmail.com>
2022-01-16 23:24:52 +0300

    add projects section

---
+++ src/components/organisms/VProjectsSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="v-project-section">
+    <div class="v-project-section__title">
+      Projects
+    </div>
+
+    <div class="v-project-section__items">
+      <v-project-card
+        v-for="project in projects"
+        :key="project.id"
+        :title="project.title"
+        :description="project.description"
+        :image="project.image"
+        :to="project.to"
+      ></v-project-card>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import VProjectCard from '@/components/molecules/VProjectCard.vue';
+
+const projects = [];
+</script>
+
+<style lang="scss" scoped>
+.v-project-section {
+  @apply grid grid-cols-1 container md:px-20 items-center p-5 md:mb-20;
+
+  &__title {
+    @apply text-5xl mb-5;
+  }
+
+  &__items {
+    @apply flex flex-wrap items-center;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,15 @@
     <v-about-me-section></v-about-me-section>
 
     <v-technologies-section></v-technologies-section>
+
+    <v-projects-section></v-projects-section>
   </div>
 </template>
 
 <script setup>
 import VAboutMeSection from '@/components/organisms/VAboutMeSection.vue';
 import VTechnologiesSection from '@/components/organisms/VTechnologiesSection.vue';
+import VProjectsSection from '@/components/organisms/VProjectsSection.vue';
 </script>
 
 <style lang="scss">
