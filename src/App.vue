aef57582ff8085f97b08f8cfc015b7b9818df965
Ibrahim Turan <hello@ibrahimturan.com>
2023-01-07 03:56:11 +0300

    feat: add new scrollbar design

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,18 @@
 <style lang="scss">
 .default-layout {
   @apply text-gray-800 min-h-screen bg-accent-50;
+
+  scrollbar-color: #C3D0DF transparent;
+  scrollbar-width: thin;
+
+  ::-webkit-scrollbar {
+    width: 5px;
+    height: 8px;
+    background-color: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #C3D0DF;
+  }
 }
 </style>
