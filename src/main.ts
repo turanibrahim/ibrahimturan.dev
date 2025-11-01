d36daa12aac1cb447a9c568ebf9cb4d370af527a
Ibrahim Turan <hello@ibrahimturan.com>
2025-11-01 14:13:55 +0300

    feat: integrate Tailwind CSS and restructure styles

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import pinia from '@/plugins/pinia';
 import router from './router';
 import App from './app.vue';
-import '@/assets/styles/main.scss';
 
 const app = createApp(App)
   .use(router)
